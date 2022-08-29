4e8f17c2132daa04b794854cafe1721f15a44c88
Yulia <yuliana.kliapitskaya@leverx.com>
2022-08-29 18:28:17 +0300

    remove unusefull file

--- hospital/src/api/Cards/Cards.d.ts
+++
@@ -1,6 +0,0 @@
-import { ICard } from 'src/redux/types/cardsTypes'
-import { IService } from 'src/redux/types/servicesTypes'
-
-export type CardsInfoResponse = ICard
-
-export type ServicesInfoResponse = IService
--- hospital/src/api/Cards/Cards.ts
+++ hospital/src/api/Cards/Cards.ts
@@ -1,17 +1,18 @@
 import { HTTP_METHODS } from 'src/helper/api'
+import { ICard } from 'src/redux/types/cardsTypes'
+import { IService } from 'src/redux/types/servicesTypes'
 import Fetcher from 'src/services/fetcher'
-import { CardsInfoResponse, ServicesInfoResponse } from './Cards.d'
 
 const fetcher = new Fetcher()
 
 export const requestCardsInfo = () =>
-  fetcher.requestToReceive<{}, CardsInfoResponse[]>({
+  fetcher.requestToReceive<{}, ICard[]>({
     url: 'service-requests/calendar/events/?event_date_after=1971-02-24T00:00&event_date_before=2200-02-24T00:00&grouping=true',
     method: HTTP_METHODS.GET
   })
 
 export const requestServiceType = () =>
-  fetcher.requestToReceive<{}, ServicesInfoResponse[]>({
+  fetcher.requestToReceive<{}, IService[]>({
     url: 'service-requests/types/',
     method: HTTP_METHODS.GET
   })
