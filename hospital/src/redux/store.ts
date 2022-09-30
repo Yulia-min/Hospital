8d72d37890ddb2a5963024e47faaa8cd0132b84b
Yulia <yuliana.kliapitskaya@leverx.com>
2022-09-30 17:39:19 +0300

    add comma

--- hospital/src/redux/store.ts
+++ hospital/src/redux/store.ts
@@ -13,7 +13,7 @@ export const store = configureStore({
     patientWithSymptoms: patientsSlice,
     patientsDate: patientsSlice,
     choosenRequestType: patientsSlice,
-    currentPatient: patientsSlice
+    currentPatient: patientsSlice,
     requestDetails: cardsSlice
   }
 })
